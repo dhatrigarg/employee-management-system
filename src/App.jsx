14c08dda6bd3e6d656d083c6bdecd26e03506ee4
dhatrigarg <dhatrigarg67@gmail.com>
2026-07-28 11:39:28 +0530

    setting up dashboard on login

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,38 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import Login from './components/Auth/Login'
 import EmployeeDashboard from './components/Dashboard/EmployeeDashboard'
 import AdminDashboard from './components/Dashboard/AdminDashboard'
 import { getLocalStorage, setLocalStorage } from './utils/localStorage'
+import { useContext } from 'react'
+import { AuthContext } from './context/AuthProvider'
 
 
 const App = () => {
 
-  useEffect(() => {
-    setLocalStorage()
-    getLocalStorage()
-  },[])
+  // useEffect(() => {
+    //   setLocalStorage()
+    //   getLocalStorage()
+    // },[])
+    
+  const authData = useContext(AuthContext)
   
+  const [user, setUser] = useState(null)
+
+  const handleLogin = (email,password) => {
+    if(email == 'admin@example.com' && password == 123){
+      setUser('admin')
+    } else if (authData && authData.employees.find((e)=>email == e.email && e.password == password)){
+      setUser('employee')
+    }else{
+      alert("Invalid credentials")
+    }
+  }
+  
+
   return (
     <>
-    <Login/>
-    {/* <EmployeeDashboard/> */}
-    {/* <AdminDashboard/> */}
+    {!user ? <Login handleLogin={handleLogin}  /> : ''}
+    {user == 'admin' ?<AdminDashboard/> : <EmployeeDashboard/>}
     </>
   )
 }
--- src/utils/localStorage.jsx
+++ src/utils/localStorage.jsx
@@ -265,6 +265,5 @@ export const getLocalStorage = () => {
     const employees = JSON.parse( localStorage.getItem('employees'))
     const admin = JSON.parse( localStorage.getItem('admin'))
 
-    console.log(employees)
-    console.log(admin)
+    return {employees, admin}
 }
